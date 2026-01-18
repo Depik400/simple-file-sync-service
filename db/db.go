8481c0bd033ad2d61f24149c158b41337ec51c62
Pavel Kononov <pro100cuber@gmail.com>
2026-01-18 14:29:18 +0700

    feat: replace sqlite with pure go version

--- db/db.go
+++ db/db.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"time"
 
-	_ "github.com/mattn/go-sqlite3"
+	_ "modernc.org/sqlite"
 )
 
 type FileRecord struct {
@@ -24,7 +24,7 @@ type Database struct {
 }
 
 func NewDatabase(dbPath string) (*Database, error) {
-	db, err := sql.Open("sqlite3", dbPath)
+	db, err := sql.Open("sqlite", dbPath)
 	if err != nil {
 		return nil, fmt.Errorf("failed to open database: %w", err)
 	}
